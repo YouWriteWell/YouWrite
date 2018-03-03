ca9468b4fe9fcf6a42452af1adffc8358e58b135
nhaouari <nhaouari@usthb.dz>
2018-03-03 14:29:47 +0100

    V.1.0.0.0

--- Youwrite/Properties/AssemblyInfo.cs
+++ Youwrite/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YouWrite")]
-[assembly: AssemblyDescription("To Make Writing Easier")]
+[assembly: AssemblyDescription("Make Writing Easier")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("YouWrite")]
 [assembly: AssemblyProduct("YouWrite")]
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93063280-6a5a-44fe-978c-614ce7433ea3")]
